a2ced1b0b27e40ddc2efb86d1239428108b1465f
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-18 19:43:16 +0100

    App is finished

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ const result = document.getElementById("result");
 let temp;
 
 function convert() {
-    pass
+    if (toFahrenheit.checked) {
+        temp = parseInt(textBox.value);
+        temp = (temp * 9/5) + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    } else if (toCelsius.checked) {
+        temp = parseInt(textBox.value);
+        temp = (temp - 32) * 5/9;
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else {
+        result.textContent = "Please select a unit.";
+    }
 }
