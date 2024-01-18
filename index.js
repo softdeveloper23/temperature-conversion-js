4238f2fb6d42082ea49309cac61c3235230097ed
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-18 19:25:50 +0100

    Added const and let variables

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Temperature Conversion Program
 
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
 function convert() {
     pass
 }
